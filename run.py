35312f63ff1b6954a7870389567e55924618602e
Takuya Nakamura <takuyanakamura@TakuyanoMacBook-Pro.local>
2020-07-10 15:44:00 +0900

    template engine

--- run.py
+++ run.py
@@ -7,7 +7,10 @@ app.register_blueprint(sub_module)  # blueprint
 
 @app.route("/")  # どのページで実行する関数か設定
 def main():
-    return "Hello, World!"  # Hello, World! を出力
+    # return "Hello, World!"  # Hello, World! を出力
+    # templatesフォルダは記載しなくてよい
+    param = {'name': "takuya_para,", 'age': 100}
+    return render_template("index.html", name="takuya", age=20, param=param)
 
 
 @app.route("/<name>")
